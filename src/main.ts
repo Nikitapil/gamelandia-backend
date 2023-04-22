503cb6b122c2d8e32027902da1e70aff1179600e
Nikitapil <nikitapiliavets@gmail.com>
2023-04-22 21:13:02 +0300

    add base configuration for gamelandia-backend

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function start() {
+  const PORT = process.env.PORT || 5000;
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Gamelandia api')
+    .setDescription('Gamelandia api methods')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  app.enableCors({
+    origin: process.env.CLIENT_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  );
+
+  app.use(cookieParser());
+
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+}
+start();
