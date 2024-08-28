73f77e6a2ba6f9ba4a68e9e72e0e0eb56a6f1e87
Nikitapil <nikitapiliavets@gmail.com>
2024-08-28 19:29:58 +0300

    fix schema error for ManyCommentsReturnDto

--- src/quiz-comments/dto/ManyCommentsReturnDto.ts
+++ src/quiz-comments/dto/ManyCommentsReturnDto.ts
@@ -2,7 +2,7 @@ import { QuizCommentReturnDto } from './QuizCommentReturnDto';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class ManyCommentsReturnDto {
-  @ApiProperty({ description: 'comments list', type: QuizCommentReturnDto })
+  @ApiProperty({ description: 'comments list', type: [QuizCommentReturnDto] })
   comments: QuizCommentReturnDto[];
 
   @ApiProperty({ description: 'total comments count', type: Number })
