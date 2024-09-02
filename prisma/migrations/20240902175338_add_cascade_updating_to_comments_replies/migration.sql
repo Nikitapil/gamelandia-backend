8160c67a7b943ed3e97d3ff73855154e7a38c6f7
Nikitapil <nikitapiliavets@gmail.com>
2024-09-02 20:57:25 +0300

    add migrations for cascade delete quizComments

---
+++ prisma/migrations/20240902175338_add_cascade_updating_to_comments_replies/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "QuizComment" DROP CONSTRAINT "QuizComment_replyToId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "QuizComment" ADD CONSTRAINT "QuizComment_replyToId_fkey" FOREIGN KEY ("replyToId") REFERENCES "QuizComment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
